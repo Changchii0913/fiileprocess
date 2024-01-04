5ac8eb167c362a01a5cdf9badc29e5df00c9ad4f
Changchii0913 <bet654321@gmail.com>
2024-01-04 10:18:47 +0800

    0104 file copy/read

--- src/main/java/Chang/Main.java
+++ src/main/java/Chang/Main.java
@@ -1,11 +1,5 @@
 package Chang;
 
-import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
